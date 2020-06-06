a89bd3ee230308e316d9d536f036293e73aad8d7
Ankit Sharma <48281291+sharmaister@users.noreply.github.com>
2020-06-06 14:39:55 +0530

    Add files via upload

--- js/services.js
+++ js/services.js
@@ -4,10 +4,10 @@ function services() {
   <h2>SERVICES</h2>
   <h4>What we offer</h4>
   <br>
-  <div class="row slideanim">
+  <div class="row ">
   
-    <div class="card-deck">
-          <div class="card mb-2">
+    <div class="card-deck ">
+          <div class="card mb-2 mx-auto">
               
               <div class="card-body"><span class="fas fa-book logo-small"></span>
                   <h4 class="card-title text-center">Card title</h4>
@@ -15,35 +15,35 @@ function services() {
                   <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
               </div>
           </div>
-          <div class="card mb-2">
+          <div class="card mb-2 mx-auto">
               
             <div class="card-body"><span class="fas fa-book logo-small"></span>
                 <h4 class="card-title text-center">Card title</h4>
                 <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                 <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
             </div>
-        </div> <div class="card mb-2">
+        </div> <div class="card mb-2 mx-auto">
               
           <div class="card-body"><span class="fas fa-book logo-small"></span>
               <h4 class="card-title text-center">Card title</h4>
               <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
               <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
           </div>
-      </div> <div class="card mb-2">
+      </div> <div class="card mb-2 mx-auto">
               
               <div class="card-body"><span class="fas fa-book logo-small"></span>
                   <h4 class="card-title text-center">Card title</h4>
                   <p class="card-text">This  longer.</p>
                   <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
               </div>
-          </div> <div class="card mb-2">
+          </div> <div class="card mb-2 mx-auto">
               
             <div class="card-body"><span class="fas fa-book logo-small"></span>
                 <h4 class="card-title text-center">Card title</h4>
                 <p class="card-text">This is a wider card with supporting text below as a .</p>
                 <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
             </div>
-        </div> <div class="card mb-2">
+        </div> <div class="card mb-2 mx-auto">
               
           <div class="card-body"><span class="fas fa-book logo-small"></span>
               <h4 class="card-title text-center">Card title</h4>
